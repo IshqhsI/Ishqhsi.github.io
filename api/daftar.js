3dedfa76f98c07c94f751b6373cc945ead1d9bce
Muhammad Ridhwan <107637984+IshqhsI@users.noreply.github.com>
2024-06-04 00:54:59 +0800

    Update daftar.js

--- api/daftar.js
+++ api/daftar.js
@@ -1,8 +1,48 @@
 const fs = require('fs');
 
 const loaddata = () => {
-    let data = fs.readFileSync('./data/daftar.json', 'utf-8');
-    data = JSON.parse(data);
+    // let data = fs.readFileSync('./data/daftar.json', 'utf-8');
+    // data = JSON.parse(data);
+    data = [
+    {
+      "nama": "John Doe",
+      "nim": "123456789",
+      "jurusan": "Teknik Informatika",
+      "angkatan": 2019,
+      "ipk": 3.75
+    },
+    {
+      "nama": "Jane Smith",
+      "nim": "987654321",
+      "jurusan": "Sistem Informasi",
+      "angkatan": 2020,
+      "ipk": 3.85
+    },
+    {
+      "nama": "Alice Johnson",
+      "nim": "456789123",
+      "jurusan": "Teknik Elektro",
+      "angkatan": 2018,
+      "ipk": 3.9
+    },
+    {
+      "nama": "Bob Brown",
+      "nim": "321654987",
+      "jurusan": "Teknik Industri",
+      "angkatan": 2017,
+      "ipk": 3.6
+    },
+    {
+      "nama": "Michael Lee",
+      "nim": "654321987",
+      "jurusan": "Matematika",
+      "angkatan": 2021,
+      "ipk": 3.95
+    }
+  ]
+
+
+    
 
     return data;
 }
