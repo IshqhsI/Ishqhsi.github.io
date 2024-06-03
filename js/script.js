f3670f88f69d94eb5408df2a1551a140250362a7
IshqhsI <oneridh18@gmail.com>
2024-06-03 23:41:18 +0800

    rename folder backend jadi api & buat vercel.json

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 import { createApp } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js';
+// import test from 'node:test';
 
 createApp({
   data() {
@@ -7,12 +8,31 @@ createApp({
       jalan:
         'Jl. Kuripan No.21 A, RT.03, Kuripan, Kec. Banjarmasin Tim., Kota Banjarmasin, Kalimantan Selatan',
       linkMap: 'https://maps.app.goo.gl/3YwVxzGNhZDoG2Hy8',
+
+      datadaftar: []
     };
   },
 
   mounted() {
     axios.get('http://localhost:3000/test').then((response) => {
-      console.log(response.data);
+      this.datadaftar = JSON.parse(JSON.stringify(response.data));
+
+      console.log(this.datadaftar);
     });
+
+
+  },
+
+  methods: {
+    getdata() {
+      // alert('tes');
+
+      let test = this.datadaftar;
+      console.log(test);
+      test.forEach(element => {
+        alert('tes');
+        console.log(element);
+      });
+    },
   },
 }).mount('#app');
